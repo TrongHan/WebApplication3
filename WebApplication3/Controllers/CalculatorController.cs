37ea0b717cc2d3ad95858b63e1aa221d3932ac99
TrongHan <56809918+TrongHan@users.noreply.github.com>
2021-10-19 14:06:41 +0700

    Initial commit

---
+++ WebApplication3/Controllers/CalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("My view");
+        }
+        public IActionResult chuyendolathanhdong(float a)
+        {
+            Models.Calculator cal = new Models.Calculator(a);
+            cal.chuyendolathanhdong();
+            return View("My view",cal);
+        }
+        
+    }
+}
---
+++ WebApplication3/Models/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Calculator
+    {
+        public float dola;
+        
+        public object result=0;
+
+        public Calculator(float a)
+        {
+            this.dola = a;
+            
+        }
+        public void chuyendolathanhdong()
+        {
+            result = dola * 22660;
+        }
+        
+    }
+}
